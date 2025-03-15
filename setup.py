6cf06085a83b40d75befbd25ce49f55afbd9685f
piotr <nwg.piotr@gmail.com>
2025-03-15 03:26:13 +0100

    fix package_data

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        "": ["langs/*", "icons/dark*", "icons/light*"]
+        "": ["langs/*", "icons/dark/*", "icons/light/*"]
     },
     url='https://github.com/nwg-piotr/nwg-shell-config',
     license='GPL3',
